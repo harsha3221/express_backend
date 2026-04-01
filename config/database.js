68fa1bac98f0d58281e55b996ca4b942b63e365a
harsha3221 <harshadhulipalla6@gmail.com>
2026-04-01 10:10:26 +0530

    after adding port config for db

--- config/database.js
+++ config/database.js
@@ -5,11 +5,17 @@ const db = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    // --- THE FIX ---
+    port: process.env.DB_PORT || 21521,
+    // ---------------
     ssl: {
         rejectUnauthorized: false
     },
+    waitForConnections: true,
     connectionLimit: Number(process.env.DB_CONNECTION_LIMIT) || 15,
-    queueLimit: Number(process.env.DB_QUEUE_LIMIT) || 15
+    queueLimit: Number(process.env.DB_QUEUE_LIMIT) || 15,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 10000
 });
 
 module.exports = db.promise();
